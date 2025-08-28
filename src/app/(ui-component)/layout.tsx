2cd9d367146cd5f2d82f1f5bd141383ed5b8ba82
Raj <raj.kv@relyonsoft.com>
2025-08-28 18:37:52 +0530

    chnages

--- src/app/(ui-component)/layout.tsx
+++ src/app/(ui-component)/layout.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-export default function UIComponentLayout() {
-  return <main id="ui-component">UIComponentLayout</main>;
+export default function UIComponentLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main id="ui-component">
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <section id="ui-component-section">{children}</section>
+        </SidebarInset>
+      </SidebarProvider>
+    </main>
+  );
 }
